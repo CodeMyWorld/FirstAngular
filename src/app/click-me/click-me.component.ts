51829b1394c79c295899c66a0c16329158596884
Alex Lai <alexlai@softlab.cs.tsukuba.ac.jp>
2017-01-16 17:50:48 +0900

    http observable

--- src/app/click-me/click-me.component.ts
+++ src/app/click-me/click-me.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpServiceService } from '../http-service.service'
 import {Hero} from '../hero'
-
+import { Currency } from '../currency'
 @Component({
   selector: 'click-me',
   templateUrl: './click-me.component.html',
   styleUrls: ['./click-me.component.css']
 })
+
 export class ClickMeComponent implements OnInit {
   clickMessage = 'havent click yet';
   values = '';
   constructor(private httpService: HttpServiceService) { }
+  currencies: Currency[];
+  errorMessage: string
 
   ngOnInit() {
   }
 
   onClickMe(){
-    this.httpService.getHeroes().then(heroes => this.clickMessage = heroes);
+    var response;
+    this.httpService.getHeroes().then(heroes => this.values = heroes.toString());
+
+    this.httpService.getCurrencies().subscribe(
+      currencies => this.currencies = currencies,
+      errorMessage => this.errorMessage = <any>errorMessage
+    )
   }
   onKey(value: string){
     this.values += value + ' | ';
--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http'
 import { Hero } from './hero'
 import 'rxjs/add/operator/toPromise';
+import './rxjs-operators'
+import { Observable } from 'rxjs/Observable'
+import { Currency } from './currency'
 
 @Injectable()
 export class HttpServiceService {
-  private url = "http://localhost:8080/api/currencies";
+  private url = "/api/currencies";
 
   constructor(private http: Http) {}
 
   getHeroes(): Promise<string> {
     return this.http.get(this.url)
                .toPromise()
-               .then(response => response.json() as string)
+               .then(response => response.json())
                .catch(this.handleError);
   }
 
@@ -20,4 +23,17 @@ export class HttpServiceService {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);  
  }
+
+ private extractData(res: Response){
+   let body = res.json();
+   return body.result.currencies || {};
+ }
+
+ getCurrencies(): Observable<Currency[]>{
+   return this.http.get(this.url)
+   .map(this.extractData)
+   .catch(this.handleError);
+ }
+
+
 }
